d6a59916667b8dac35668a7767d2db332050250e
transportians-devteam <trans.edteam@gmail.com>
2026-09-03 11:50:52 +0530

    vercel ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  vite: {
-    server: {
-      port: 3000,
-      strictPort: true,
-    },
-    preview: {
-      port: 3000,
-      strictPort: true,
-    },
+  server: {
+    port: 3000,
+    strictPort: true,
   },
-  nitro: {
-    preset: "vercel",
+  preview: {
+    port: 3000,
+    strictPort: true,
   },
-  tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
-    server: { entry: "server" },
-  },
+  plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
+    tanstackStart({ server: { entry: "server" } }),
+    nitro({ preset: "vercel" }),
+    react(),
+  ],
 });
